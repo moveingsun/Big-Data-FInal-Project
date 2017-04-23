262dff30a1929cf557f7a3c0d714fd41f3c61ca6
isillidril <zhang.zhan2@husky.neu.edu>
2017-04-23 14:12:55 -0400

    Add files via upload

--- audio_transcribe.py
+++ audio_transcribe.py
@@ -7,6 +7,9 @@ from interface import ModelInterface
 from utils import read_wav
 import pygame
 import sys
+import os
+import time
+import random
 
 AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "audio/zhu1.wav")
 
@@ -19,6 +22,7 @@ src = r.recognize_google(audio)
 
 # recognize speech using Google Speech Recognition
 try:
+    rand = random.randint(1, 9)
     m = ModelInterface.load(path.join(path.dirname(path.realpath(__file__)), "model.out"))
     fs, signal = read_wav(AUDIO_FILE)
     newName = m.predict(fs, signal)
@@ -30,61 +34,131 @@ try:
     name_str = name.decode('utf8')
     if newName in name_str:
         print("Hi, old friend!")
+        os.system("espeak 'Hi, old friend!' &")
+        time.sleep(2)
     elif name_str != newName:
         print("Hi, " + newName + "!")
-
-    if 'hello' in src or 'hi' in src:
-        print("Hi!")
-    elif 'weather' and 'what' in src:
-        if 'today' not in src and 'tomorrow' not in src:
-            print("Please specific the date.")
-        elif 'today' in src:
-            google = 'weather'
-            print("Searching on the Internet...")
-        # webbrowser.open('http://www.google.com/search?btnG=1&q=%s'%google)
+        if newName == "Menglu":
+            os.system("espeak 'Hi, Menglu' &")
+        elif newName == "Zhan":
+            os.system("espeak 'Hi, Zhan' &")
+        elif newName == "Yuxing":
+            os.system("espeak 'Hi, Yuxing' &")
+        elif newName == "Yankai":
+            os.system("espeak 'Hi, Yankai' &")
         else:
-            google = 'weather tomorrow'
-            print("Searching on the Internet...")
-            webbrowser.open('http://www.google.com/search?btnG=1&q=%s' % google)
-    elif 'myneu' in src:
-        print("Searching on the Internet...")
-        webbrowser.open('https://myneu.neu.edu/cp/home/displaylogin')
-    elif 'your' in src and 'favourite' in src and 'color' in src or 'colour' in src:
-        print("My favourite color is bule, what about you?")
-    elif 'favourite' in src and 'song' in src:
-        pygame.init()
-        pygame.mixer.init()
-    # screen = pygame.display.set_mode([640,480])
-    # pygame.mixer.music.set_volume(0.5)
-        END = pygame.USEREVENT + 1
-        pygame.mixer.music.set_endevent(END)
-        track = pygame.mixer.music.load("english.wav")
-        pygame.mixer.music.play()
-        pygame.mixer.music.fadeout(20000)
+            os.system("espeak 'Hi' &")
+        time.sleep(2)
 
-        while 1:
-            for event in pygame.event.get():
-                if event.type == END:
-                    sys.exit()
-    elif 'big' in src and 'data' in src:
-        print("Love you! Dino!")
-    elif 'record' in src and 'voice' in src:
-        f = open("temp.txt", "rb+")
-        f.truncate(0)
-        f.write(bytes(src, 'utf8'))
-        f.close()
-    elif 'How' in src and 'are' in src and 'you' in src and 'old' not in src:
-        print("I am fine, thank you, and you? :-)")
-    else:
-        f = open("temp.txt", "rb+")
-        f.truncate(0)
-        f.write(bytes(src, 'utf8'))
-        f.close()
+        if 'hello' in src or 'hi' in src:
+            print("Hi! " + newName)
+            if newName == "Menglu":
+                os.system("espeak 'Hi, Menglu' &")
+            elif newName == "Zhan":
+                os.system("espeak 'Hi, Zhan' &")
+            elif newName == "Yuxing":
+                os.system("espeak 'Hi, Yuxing' &")
+            elif newName == "Yankai":
+                os.system("espeak 'Hi, Yankai' &")
+            else:
+                os.system("espeak 'Hi' &")
+            time.sleep(2)
+        elif 'weather' in src and 'what' in src:
+            if 'today' not in src and 'tomorrow' not in src:
+                print("Please specific the date.")
+                os.system("espeak 'Please specific the date.' &")
+                time.sleep(2)
+            elif 'today' in src:
+                google = 'weather'
+                print("Searching on the Internet...")
+                os.system("espeak 'Searching on the Internet' &")
+                time.sleep(2)
+                webbrowser.open('http://www.google.com/search?btnG=1&q=%s' % google)
+            else:
+                google = 'weather tomorrow'
+                print("Searching on the Internet...")
+                os.system("espeak 'Searching on the Internet' &")
+                webbrowser.open('http://www.google.com/search?btnG=1&q=%s' % google)
+        elif 'my' in src and 'neu' in src or 'Neu' in src:
+            print("Searching on the Internet...")
+            os.system("espeak 'Searching on the Internet' &")
+            webbrowser.open('https://myneu.neu.edu/cp/home/displaylogin')
+        elif 'your' in src and 'favorite' in src and 'color' in src or 'colour' in src:
+            print("My favourite color is bule, what about you?")
+            os.system("espeak 'My favourite color is bule, what about you?' &")
+        elif 'favourite' in src or 'song' in src:
+            pygame.init()
+            pygame.mixer.init()
+            END = pygame.USEREVENT + 1
+            pygame.mixer.music.set_endevent(END)
+            track = pygame.mixer.music.load("audio/english.wav")
+            pygame.mixer.music.play()
+            pygame.mixer.music.fadeout(20000)
+
+            while 1:
+                for event in pygame.event.get():
+                    if event.type == END:
+                        sys.exit()
+        elif 'big' in src and 'data' in src:
+            print("Love you! Dino!")
+            os.system("espeak 'Love you! Dino!' &")
+            time.sleep(2)
+        elif 'record' in src and 'voice' in src:
+            f = open("tempfile.txt", "rb+")
+            f.truncate(0)
+            f.write(bytes(src, 'utf8'))
+            f.close()
+            print("Please open the 'tempfile.txt' to see your voice.")
+        elif 'how' in src and 'are' in src and 'you' in src and 'old' not in src:
+            print("I am fine, thank you, and you? :-)")
+            os.system("espeak 'I am fine, thank you, and you' &")
+            time.sleep(2)
+        else:
+            f = open("tempfile.txt", "rb+")
+            f.truncate(0)
+            f.write(bytes(src, 'utf8'))
+            f.close()
+            if rand == 1:
+                print("Sorry, I can't understand you.")
+                os.system("espeak 'Sorry, I can't understand you.' &")
+                time.sleep(2)
+            elif rand == 2:
+                print("Oh go on.")
+                os.system("espeak 'Oh go on.' &")
+                time.sleep(2)
+            elif rand == 3:
+                print("Once again :-)")
+                os.system("espeak 'Once again' &")
+                time.sleep(2)
+            elif rand == 4:
+                print("I never really thought about it.")
+                os.system("espeak 'I never really thought about it.' &")
+                time.sleep(2)
+            elif rand == 5:
+                print("Maybe you can ask others.")
+                os.system("espeak 'Maybe you can ask others.' &")
+                time.sleep(2)
+            elif rand == 6:
+                print("Simple question.")
+                os.system("espeak 'Simple question.' &")
+                time.sleep(2)
+            elif rand == 7:
+                print("I am out of working.")
+                os.system("espeak 'I am out of working.' &")
+                time.sleep(2)
+            elif rand == 8:
+                print("I'd rather not.")
+                os.system("espeak 'I'd rather not.' &")
+                time.sleep(2)
+            else:
+                print("Make it clear.")
+                os.system("espeak 'Make it clear.' &")
+                time.sleep(2)
+
 
 
 except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
+    print("Sorry I don't understand.")
+    os.system("espeak 'Sorry I don't understand.' &")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-
